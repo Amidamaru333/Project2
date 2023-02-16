067dfa3766f605a87bf3c8a7c19773caeea19ee1
georgeort <124184190+georgeort@users.noreply.github.com>
2023-02-16 21:43:03 +0300

    Second task

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,8 +1,14 @@
 ﻿//First Task
-int number = Convert.ToInt32(Console.ReadLine());
-if(number<1000 && number>99){
-    Console.WriteLine((number%100)/10);
-}else{
-    Console.WriteLine("Число не является трёхзначным");
-}
+// int number = Convert.ToInt32(Console.ReadLine());
+// if(number<1000 && number>99){
+//     Console.WriteLine((number%100)/10);
+// }else{
+//     Console.WriteLine("Число не является трёхзначным");
+// }
 //Second Task
+int number0 = Convert.ToInt32(Console.ReadLine());
+if (number0<10000000 && number0 > 999999){
+    Console.WriteLine((number0/10000)%10);
+}else{
+    Console.WriteLine("третьей цифры нет");
+}

51d65c67df1531c087b658d52d370322190b7074
Dmitry <doc9830@rambler.ru>
2023-02-16 21:17:43 +0300

    Добавлена 1 задача

---
+++ dz2/Program.cs
@@ -0,0 +1,8 @@
+﻿//First Task
+int number = Convert.ToInt32(Console.ReadLine());
+if(number<1000 && number>99){
+    Console.WriteLine((number%100)/10);
+}else{
+    Console.WriteLine("Число не является трёхзначным");
+}
+//Second Task
